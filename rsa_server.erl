717d58ead4ab0270d4225139dc27e3524f88a041
Aubrey Anderson <aubrey@bigsur.com>
2016-12-02 16:11:28 -0500

    changed code to work with broker_crypto fixed db calls no longer needs db ref

--- rsa_server.erl
+++ rsa_server.erl
@@ -1,7 +1,5 @@
 -module(rsa_server).
--import(public_key, [encrypt_private/2, encrypt_public/2, decrypt_private/2,
-                     decrypt_public/2, generate_key/1]).
--export([init/1, handle_call/3, handle_cast/2, get_encrypt_send/5,
+-export([init/1, handle_call/3, handle_cast/2, get_encrypt_send/3,
          code_change/3, terminate/2, handle_info/2]).
 -export([register/1, get_key/1, get_nodes/0, get_server_key/0]).
 -export([start/0, stop/0]).
@@ -50,35 +48,33 @@ init(_) ->
     [{PubKey, _}] = public_key:ssh_decode(PubBin, public_key),
     [PrivKeyEntry] = public_key:pem_decode(PrivBin),
     PrivKey = public_key:pem_entry_decode(PrivKeyEntry),
-    {ok, DB} = db:start_link(),
-    {ok, {{PubKey, PrivKey}, DB}}.
+    ok = db:start_link(),
+    {ok, {PubKey, PrivKey}}.
 
-handle_cast({register, Node, Data}, {{PubKey, PrivKey}, DB}) -> 
-    KeyBin = decrypt_private(Data, PrivKey),
+handle_cast({register, Node, Envelope}, {PubKey, PrivKey}) -> 
+    KeyBin = broker_crypto:extract_message(Envelope, PrivKey),
     Key = binary_to_term(KeyBin),
-    db:insert(DB, {Node, Key}),
-    {noreply, {{PubKey, PrivKey}, DB}}.
+    db:insert({Node, Key}),
+    {noreply, {PubKey, PrivKey}}.
 
 handle_call({get_key, Node}, From, State) ->
-    {{_, PrivKey}, DB} = State,
-    Args = [get_key, Node, From, PrivKey, DB],
+    Args = [get_key, Node, From],
     spawn(rsa_server, get_encrypt_send, Args),
     {noreply, State};
 
 handle_call(get_server_key, _, State) ->
-    {{PubKey, _}, _} = State,
+    {PubKey, _} = State,
     {reply, PubKey, State};
 
 handle_call(get_nodes, From, State) ->
-    {{_, PrivKey}, DB} = State,
-    Args = [get_nodes, {}, From, PrivKey, DB],
+    Args = [get_nodes, {}, From],
     spawn(rsa_server, get_encrypt_send, Args),
     {noreply, State}.
 
 handle_info(_Info, State) -> {noreply, State}.
 
-terminate(_Reason, {_, DB}) ->
-    gen_server:stop(DB).
+terminate(_, _) ->
+    db:stop().
 
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
 
@@ -86,12 +82,13 @@ code_change(_OldVsn, State, _Extra) -> {ok, State}.
 %% Internal Function Definitions
 %% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-get_encrypt_send(Call, Name, To, PrivKey, DB) -> 
+get_encrypt_send(Call, Name, To) -> 
     Result = case Call of
-        get_key -> db:get_key(DB, Name);
-        get_nodes -> db:get_nodes(DB)
+        get_key -> db:get_key(Name);
+        get_nodes -> db:get_nodes()
     end,
     Message = term_to_binary(gen_server:call(Result)),
-    ClientKey = db:get_key(DB, node(To)),
-    CText = encrypt_private(encrypt_public(Message, ClientKey), PrivKey),
-    gen_server:reply(To, CText).
+    ClientKey = db:get_key(node(To)),
+    Envelope = broker_crypto:build_envelope(ClientKey, Message),
+    gen_server:reply(To, Envelope).
+
