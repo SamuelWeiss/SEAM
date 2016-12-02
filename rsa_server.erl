5f68ecc29ae09b0911a560c9dc6fa0bb4291a947
Aubrey Anderson <aubrey@bigsur.com>
2016-12-02 14:13:50 -0500

    Added function to return the server's public key fixed db start_link call changed server start_link to start

--- rsa_server.erl
+++ rsa_server.erl
@@ -3,8 +3,8 @@
                      decrypt_public/2, generate_key/1]).
 -export([init/1, handle_call/3, handle_cast/2, get_encrypt_send/5,
          code_change/3, terminate/2, handle_info/2]).
--export([register/1, get_key/1, get_nodes/0]).
--export([start_link/0, stop/0]).
+-export([register/1, get_key/1, get_nodes/0, get_server_key/0]).
+-export([start/0, stop/0]).
 
 -behaviour(gen_server).
 
@@ -12,10 +12,14 @@
 %% API Function Definitions
 %% ~~~~~~~~~~~~~~~~~~~~~~~~
 
-start_link() ->
-    gen_server:start_link({global, ?MODULE}, ?MODULE, [], []).
+start() ->
+    gen_server:start({global, ?MODULE}, ?MODULE, [], []).
 
-stop() -> gen_server:terminate(ok, {global, ?MODULE}).
+stop() -> 
+    application:stop(asn1),
+    application:stop(crypto),
+    application:stop(public_key),
+    gen_server:stop({global, ?MODULE}).
 
 %% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %% External Function Definitions
@@ -26,6 +30,9 @@ register(Data) ->
 
 get_key(Node) ->
     gen_server:call({global, ?MODULE}, {get_key, Node}).
+
+get_server_key() ->
+    gen_server:call({global, ?MODULE}, get_server_key).
 
 get_nodes() ->
     gen_server:call({global, ?MODULE}, get_nodes).
@@ -40,13 +47,15 @@ init(_) ->
     ok = application:start(public_key),
     {ok, PubBin} = file:read_file("id_rsa.pub"),
     {ok, PrivBin} = file:read_file("id_rsa"),
-    [PubKey] = public_key:ssh_decode(PubBin, public_key),
-    [PrivKey] = public_key:pem_decode(PrivBin),
-    {ok, DB} = gen_server:start_link(db, ok, []),
+    [{PubKey, _}] = public_key:ssh_decode(PubBin, public_key),
+    [PrivKeyEntry] = public_key:pem_decode(PrivBin),
+    PrivKey = public_key:pem_entry_decode(PrivKeyEntry),
+    {ok, DB} = db:start_link(),
     {ok, {{PubKey, PrivKey}, DB}}.
 
 handle_cast({register, Node, Data}, {{PubKey, PrivKey}, DB}) -> 
-    Key = decrypt_private(Data, PrivKey),
+    KeyBin = decrypt_private(Data, PrivKey),
+    Key = binary_to_term(KeyBin),
     db:insert(DB, {Node, Key}),
     {noreply, {{PubKey, PrivKey}, DB}}.
 
@@ -55,6 +64,10 @@ handle_call({get_key, Node}, From, State) ->
     Args = [get_key, Node, From, PrivKey, DB],
     spawn(rsa_server, get_encrypt_send, Args),
     {noreply, State};
+
+handle_call(get_server_key, _, State) ->
+    {{PubKey, _}, _} = State,
+    {reply, PubKey, State};
 
 handle_call(get_nodes, From, State) ->
     {{_, PrivKey}, DB} = State,
