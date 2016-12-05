013bdc386c9e1fe5f199bd1d7950666f82f0b1d7
Russell Parker <russell.parker7@gmail.com>
2016-12-05 15:47:52 -0500

    Function documentation

--- client.erl
+++ client.erl
@@ -1,10 +1,17 @@
+%%% Assumption: client is aware of the rsa_server in some way. 
+
 -module(client).
 -import(public_key, [encrypt_private/2, encrypt_public/2,
                      decrypt_private/2, decrypt_public/2]).
 -export([setup/0, setup/2, listener/1]).
 
-setup()-> setup("id_rsa.pub", "id_rsa").
 
+%% setup: takes in string paths for the client's public and private keys; does
+%% one-time start for various encryption requirements and then registers the
+%% node with the rsa_server. Two anonymous functions are returned in a tuple: 
+%% the first takes in a node, process and term and securely delivers the term
+%% to the given process on the given node. The second lists the nodes that are
+%% available to message.
 setup(PubKeyPath, PrivKeyPath) ->
     ok = application:start(asn1),
     ok = application:start(crypto),
@@ -13,11 +20,9 @@ setup(PubKeyPath, PrivKeyPath) ->
     {ok, PrivBin} = file:read_file(PrivKeyPath),
     [PubKey] = public_key:ssh_decode(PubBin, public_key),
     [PrivKey] = public_key:pem_decode(PrivBin),
-    ServerKey = rsa_server:get_server_key(),
     spawn(?MODULE, listener, [PrivKey]),
-    % TODO: ping a node connected to rsa_server
     rsa_server:register(PubKey),
-    Message   = fun(Node, Process, Term) -> 
+    Message = fun(Node, Process, Term) -> 
         Bin = term_to_binary(Term),
         Foreign = cached_key_lookup(Node),
         Envelope = broker_crypto:build_envelope(Foreign, Bin),
@@ -28,18 +33,31 @@ setup(PubKeyPath, PrivKeyPath) ->
         end,
     {Message, Get_Nodes}.
 
+%% setup: same as setup/2 but uses default pathnames for the cient's
+%% public and private keys.
+setup()-> setup("id_rsa.pub", "id_rsa").
 
+
+%% listener: spawns a caching db and loops to listen for encrypted messages,
+%% unencrypts any received messages and passes them along to the original
+%% client which ran the setup function.
 listener(PrivKey) ->
+    % register this spawned process
 	register(seam_listener, self()),
+    % initialize key caching db
 	db:start_link(in_mem),
+    % enter loop to listen for encrypted messages
 	listener_loop(PrivKey).
 
+%% listener_loop: tail-recursion loop to listen for encrypted messages, decrypt
+%% them and forwards unencrypted Erlang terms to the main process. Takes in the
+%% client's private key as the only argument to decrypt messages.
 listener_loop(PrivKey) ->
 	Decrypt = fun(Msg) ->
         Bin = broker_crypto:extract_message(Msg, PrivKey),
         binary_to_term(Bin)
         end,
-	% recieve a message
+	% recieve properly formatted message
 	receive
 		{message, Recipient, Contents} ->
 			Recipient ! Decrypt(Contents);
@@ -48,11 +66,16 @@ listener_loop(PrivKey) ->
 	end,
 	listener_loop(PrivKey).
 
+%% cached_key_lookup: given a node, opaquely return the corresponding key, 
+%% using caching when possible. If a key is not yet cached it is added
+%% to the cache when retrieved from the rsa_server.
 cached_key_lookup(Node) ->
     case db:get_key(Node) of 
+        % key is not yet cached
         [] ->
 	        Key = rsa_server:get_key(Node),
             db:insert({Node, Key}),
             Key;
+        % key is already cached
         Key -> Key
     end.
