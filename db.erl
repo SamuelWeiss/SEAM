4da41f5987bb969a7b20b9d3dec7b6cee2642da5
Samuel Weiss <samuel.weiss579470@tufts.edu>
2016-12-07 16:15:42 -0500

    partial documentation of the DB module

--- db.erl
+++ db.erl
@@ -1,11 +1,24 @@
+% db.erl
+% Sam Weiss
+% 7 December 2016
+%
+% This module implements a simplified interface for node/key storage
 -module(db).
--compile(export_all).
+
+% public interface functions 
+-export([start_link/0, start_link/1, insert/1, get_key/1, get_nodes/0]).
+
+% This file contains the schema that will be used for the Mnesia database
 -include("db_schema.hrl").
 
 %% ~~~~~~~~~~~~~~~~~~~~~~~~
 %% API Function Definitions
 %% ~~~~~~~~~~~~~~~~~~~~~~~~
 
+% Start the server
+% optionally specify whether the database should use mnesia or a list based
+% in memory implementation (when mnesia isn't present)
+% the default database backend is mnesia as it comes with most erlang installs
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, mnesia, []).
 
@@ -15,19 +28,47 @@ start_link(in_mem) ->
 start_link(mnesia) ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, mnesia, []).
 
+% stop the database, for in memory databses this resets all data
+stop() -> 
+	gen_server:cast({global, ?SERVER}, stop).
 
-stop() -> ok.
+%% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%% External Function Definitions
+%% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+% get the names of all the nodes current in the database
+% if no nodes are registered this returns an empty list
+get_nodes() ->
+        gen_server:call(?MODULE, get_nodes).
+
+% get the key associated with a given node
+% if no node by that name is registered this returns an empty list
+get_key(Name) ->
+    gen_server:call(?MODULE, {get_key, Name}).
+
+% associate a node with the provided key
+% if that node already exists in the database then its value is replaced
+insert({Name, Key}) ->
+    gen_server:cast(?MODULE, {insert_key, Name, Key}).
 
 %% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %% gen_server Function Definitions
 %% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+% Initialize the in memory database by creating an empty list
 init(in_mem) ->
     {ok, []};
 
+% Initialize the mnesia database by calling a setup function
+% that is defined below
 init(mnesia) ->
     setup_mnesia_db(),
     {ok, mnesia}.
+
+%
+% These gen_server function calls largely call functions defined below
+% For that reason the functions they call are documented.
+%
 
 handle_call(get_nodes, _From, mnesia) ->
     {reply, mnesia_get_nodes(), mnesia};
@@ -61,9 +102,12 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal Function Definitions
 %% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+% search the in memory database for all of its nodes
 list_nodes_internal(DB) ->
     lists:map(fun({Name, _}) -> Name end, DB).
 
+% Search for the key in the in memory database
+% this also selects the first occurance of a node name
 get_key_internal(Node, [ {Cur_node, Cur_key} | _Remainder_DB ])
     when Node == Cur_node -> Cur_key;
 
@@ -73,14 +117,21 @@ get_key_internal(Node, [ {_Cur_node, _Cur_key} | Remainder_DB ]) ->
 get_key_internal(_Node, []) ->
     [].
 
+% put the new node and key pair on to the front of the database
 insert_key_internal(Node, Key, DB) -> 
     [{Node, Key}|DB].
 
+% Make sure that the mnesia database is up and running
+% also make sure that a table with the correct schema exists
 setup_mnesia_db() ->
     mnesia:start(),
     mnesia:create_table(keypair,
         [{attributes, record_info(fields, keypair)}]).
 
+% compose the provided node and key into a pair that conforms to the 
+% database schema. Then create a lambda function to insert the record
+% into the database. Finally, we call that lambda function in a transaction
+% so that it happens atomically.
 mnesia_db_insert({Node, Key}) ->
     Pair = #keypair{node = Node, key = Key},
     Fun = fun() ->
@@ -88,6 +139,7 @@ mnesia_db_insert({Node, Key}) ->
     mnesia:transaction(Fun),
     mnesia.
 
+% compose a function that 
 mnesia_get_key(Node) ->
     F = fun() ->
         Key = #keypair{node = Node, key = '$1'},
@@ -103,18 +155,3 @@ mnesia_get_nodes() ->
     end,
     {atomic, Data} = mnesia:transaction(F),
     Data.
-
-
-%% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-%% External Function Definitions
-%% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-get_nodes() ->
-        gen_server:call(?MODULE, get_nodes).
-
-get_key(Name) ->
-    gen_server:call(?MODULE, {get_key, Name}).
-
-insert({Name, Key}) ->
-    gen_server:cast(?MODULE, {insert_key, Name, Key}).
-
